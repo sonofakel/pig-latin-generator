0435623cb3b45e54d1ede098dadf967c37e0181c
Guest User <Guest@epicodus-2.local>
2017-07-12 11:04:41 -0700

    start 4th spec and add consonant global variable.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 // Business logic below -->
 
 // Global variables
-  var vowels = ["a","e","i","o","u","A","E","I","O","U"]
+  var vowels = ["a","e","i","o","u","A","E","I","O","U"];
+  var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","q","r","v","w","x","y","z","B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "Q", "R", "V", "W", "X", "Y", "Z"];
 
 // Program does nothing to non-alphabetical characters, since they do not contain consonants or vowels
 
@@ -14,6 +15,10 @@ var pigLatinTranslator = function(userInput) {
     if (firstChar === vowels[i]) {
       return userInput + "way";
     }
+  }
+  for (i = 0; i < consonants.length; i++)
+  if (firstChar == consonants[i]) {
+
   }
 }
 
